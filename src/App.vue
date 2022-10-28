f9997e5c2c18fec108620ff440c5d7529c28114e
programmerShinobi <faqihpratamamuhti@gmail.com>
2022-10-28 10:03:21 +0700

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <main>
+
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+      <h1>Pinia Task</h1>
+    </header>
+
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
+    <!-- loading -->
+    <div class="loading" v-if="loading">Loading tasks...</div>
+
+    <!-- task list -->
+    <div class="task-list" v-if="filter === 'all'">
+      <p>Your have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>Your have {{ favCount }} favs left to do</p>
+      <div v-for="task in favs" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <button @click="taskStore.$reset">reset state</button>
+
+  </main>
+</template>
+
+<script>
+  import TaskDetails from './components/TaskDetails.vue'
+  import TaskForm from './components/TaskForm.vue'
+
+  import { useTaskStore } from './stores/TaskStore'
+  import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
+
+  export default {
+    components: { TaskDetails, TaskForm },
+    setup() {
+      const taskStore = useTaskStore()
+
+      const { tasks, loading, favs, totalCount, favCount } = storeToRefs(taskStore)
+
+      // fetch tasks
+      taskStore.getTasks()
+
+      const filter = ref('all')
+
+      return { taskStore, filter, tasks, loading, favs, totalCount, favCount }
+    }
+  }
+</script>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="task">
+        <h3>{{ task.title }}</h3>
+        <div class="icons">
+            <em 
+                class="material-icons"
+                @click="taskStore.deleteTask(task.id)"
+            >delete</em>
+            <em 
+                class="material-icons"
+                :class="{active: task.isFav}"
+                @click="taskStore.toggleFav(task.id)"
+            >favorite</em>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { useTaskStore } from '../stores/TaskStore'
+    
+    export default {
+        props: ['task'],
+        setup() {
+            const taskStore = useTaskStore()
+
+            return { taskStore }
+        }
+    }
+</script>
